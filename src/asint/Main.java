3bedc6ab19aee1cc07cdb95bfedce399a269aee9
Alvox12 <urda.alvaro@gmail.com>
2019-03-23 00:53:12 +0100

    modifico el analizador lexico

--- src/asint/Main.java
+++ src/asint/Main.java
@@ -10,7 +10,7 @@ import java.io.Reader;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+		Reader input = new InputStreamReader(new FileInputStream("input6.txt"));
 		AnalizadorSintactico asint = new AnalizadorSintactico(input);
 	    asint.Sp();
 	}
